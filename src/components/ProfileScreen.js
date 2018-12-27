8514e3de64ce8d5524e2cd12b49099ffa9d97b4d
Nhan <hnngo@ntu.edu.sg>
2018-12-27 21:35:15 +0800

    Restyle Profile Page

--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
 
 export default class ProfileScreen extends Component {
   render() {
     return (
-      <View>
-        <View style={styles.profileImageStyle}>
-          <FontAwesome name="image" size={350}/>
+      <View style={styles.containerStyle}>
+        <View>
+          <Ionicons name="ios-people" size={250}/>
         </View>
         <Text>Profile Screen !!!!</Text>
       </View>
@@ -16,7 +16,9 @@ export default class ProfileScreen extends Component {
 }
 
 const styles = StyleSheet.create({
-  profileImageStyle: {
+  containerStyle: {
+    flex: 1,
+    backgroundColor: "#fff",
     alignItems: 'center'
   }
 });
